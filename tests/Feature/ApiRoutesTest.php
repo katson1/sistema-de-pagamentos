be808a0c2d8250714acaef04591ac7c021a7ae7d
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-04-27 18:34:17 -0300

    tests - criando testes de userStore

--- tests/Feature/ApiRoutesTest.php
+++ tests/Feature/ApiRoutesTest.php
@@ -15,37 +15,53 @@ class ApiRoutesTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function testSuccessfulUserCreation()
+    public function test_successful_user_creation()
     {
-        $userData = [
-            'name' => 'John Doe',
-            'email' => 'johndoe@example.com',
+        $response = $this->postJson('/users', $this->userData());
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'email', 'name', 'cpf_cnpj', 'user_type', 'balance', 'created_at', 'updated_at', 'id'
+        ]);
+    }
+
+    public function test_user_creation_with_invalid_data()
+    {
+        $userData = $this->userData([
+            'name' => '', // Inválido porque não está vazio
+            'email' => 'not-an-email', // Inválido porque não é email valido
+            'cpf_cnpj' => 'invalid-cpf-cnpj', // Inválido porque não é numerico
+            'user_type' => 'invalid-type', // Inválido porque é 'common' ou 'store'
+            'balance' => 'invalid-balance', // Inválido porque não é numerico
+            'password' => 'short', // Inválido porque é muito curto
+        ]);
+
+        $response = $this->postJson('/users', $userData);
+        $response->assertStatus(404);
+        $response->assertJsonValidationErrors(['name', 'email', 'cpf_cnpj', 'user_type', 'balance', 'password']);
+    }
+
+    public function test_user_creation_with_invalid_ranges_data()
+    {
+        $userData = $this->userData([
+            'cpf_cnpj' => '123', // Inválido porque é muito curto
+            'balance' => -12, // Inválido porque é negativo
+        ]);
+
+        $response = $this->postJson('/users', $userData);
+        $response->assertStatus(404);
+        $response->assertJsonValidationErrors(['cpf_cnpj', 'balance']);
+    }
+
+    //Func para reutilizar o userData nos testes
+    private function userData(array $overrides = []): array
+    {
+        return array_merge([
+            'name' => 'Katson Teste',
+            'email' => 'teste@teste.com',
             'cpf_cnpj' => '12345678901',
             'user_type' => 'common',
             'balance' => 100,
             'password' => 'password123',
-        ];
-
-        $response = $this->postJson('/users', $userData);
-
-        $response->assertStatus(201);
-        //$response->assertJson($userData);
-    }
-
-    public function atestUserCreationWithInvalidData()
-    {
-        $userData = [
-            'name' => '', // dado inválido
-            'email' => 'not-an-email',
-            'cpf_cnpj' => 'invalid-cpf',
-            'user_type' => 'invalid-type',
-            'balance' => -10,
-            'password' => 'short',
-        ];
-
-        $response = $this->postJson('/users', $userData);
-
-        $response->assertStatus(404);
-        $response->assertJsonValidationErrors(['name', 'email', 'cpf_cnpj', 'user_type', 'balance', 'password']);
+        ], $overrides);
     }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -5,13 +5,19 @@ namespace Tests\Unit;
 use App\Models\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Database\QueryException;
 
 class UserTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_create_random_users(): void
+    public function test_example(): void
+    {
+        
+        $user = User::factory()->create();
+        $this->assertDatabaseHas('users', ['email' => $user->email]);
+    }
+
+    public function test_multiple_users(): void
     {
         $users = User::factory()->count(5)->create();
         $this->assertEquals(5, count($users));
@@ -19,58 +25,11 @@ class UserTest extends TestCase
 
     public function test_specific_user(): void
     {
-        $user = User::factory()->create();
-        $this->assertDatabaseHas('users', [
-            'name' => $user->name,
-            'email' => $user->email,
-            'cpf_cnpj' => $user->cpf_cnpj,
-            'user_type' => $user->user_type,
-            'balance' => $user->balance,
-            'password' => $user->password,
+        $storeUser = User::factory()->create([
+            'user_type' => 'store',
+            'balance' => 500.00
         ]);
+        $this->assertEquals('store', $storeUser->user_type);
+        $this->assertEquals(500.00, $storeUser->balance);
     }
-
-    public function test_user_type_must_be_either_common_or_store()
-    {
-        $this->expectException(QueryException::class);
-
-        $user = User::factory()->make([
-            'user_type' => 'invalid_type'
-        ]);
-        $user->save();
-        $user->user_type = 'common';
-        
-        $this->assertTrue($user->save());
-    }
-
-    public function test_user_email_and_cpf_cnpj_must_be_unique()
-    {
-        $this->expectException(QueryException::class);
-
-        $user1 = User::factory()->create();
-
-        $user2 = User::factory()->make([
-            'email' => $user1->email,
-            'cpf_cnpj' => $user1->cpf_cnpj
-        ]);
-    
-        $user2->save();
-    }
-
-    public function test_user_required_attribute()
-    {
-        $this->expectException(QueryException::class);
-
-        $user = User::factory()->make([
-            'name' => null,
-            'email' => null,
-            'cpf_cnpj' => null,
-            'user_type' => null,
-            'balance' => null,
-            'password' => null,
-        ]);
-
-        $user->save();
-    }
-
 }
