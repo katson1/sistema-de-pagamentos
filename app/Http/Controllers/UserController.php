be5f507f8554313d9bf2f89bb5ef5d8871b2461f
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-04-26 22:44:50 -0300

    user - criando service para o usuario e mudando a logica de retorno das classes filhas do user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,38 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Models\CommonUser;
-use App\Models\StoreUser;
 use App\Http\Requests\UserStoreRequest;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function store(UserStoreRequest $request)
     {
-        $user = $this->createUserByType($request->user_type);
-
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->cpf_cnpj = $request->cpf_cnpj;
-        $user->balance = $request->balance ?? 0;
-        $user->password = bcrypt($request->password);
-        $user->save();
-
+        $user = $this->userService->createUser($request);
         return response()->json($user, 201);
     }
-
-    private function createUserByType($type): User
-    {
-        switch ($type) {
-            case 'common':
-                return new CommonUser();
-            case 'store':
-                return new StoreUser();
-            default:
-                throw new \InvalidArgumentException("Invalid user type specified");
-        }
-    }
 }
-
